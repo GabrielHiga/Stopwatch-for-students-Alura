2b7c1ee5706017f4c81f6eb755ae5162f4823f5b
Your Name <gabriel_hideki@live.com>
2022-10-28 12:10:43 -0300

    _

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,6 +1,14 @@
 import style from './Relogio.module.scss'
 
-export default function Relogio() {
+interface Props {
+    tempo: number | undefined
+}
+
+export default function Relogio({ tempo = 0 }: Props) {
+    const minutos = Math.floor(tempo / 60);
+    const segundos = tempo % 60;
+    const [minutoDezena, minutoUnidade] = String(minutos);
+    const [segundoDezena, segundoUnidade] = String(segundos);
     return (
         <>
             <span className={style.relogioNumero}>0</span>
